595259dd9ddcb8a6e286d9d0a1e0499fcdd439f6
walbourn_cp <SND\walbourn_cp>
2014-09-05 11:51:25 -0700

    Fixed whitespace problem in header file

--- Inc/XboxDDSTextureLoader.h
+++ Inc/XboxDDSTextureLoader.h
@@ -40,14 +40,14 @@ namespace Xbox
         DDS_ALPHA_MODE_CUSTOM        = 4,
     };
 
-    HRESULT __cdecl CreateD DSTextureFromMemory( _In_ ID3D11DeviceX* d3dDevice,
-                                                 _In_reads_bytes_(ddsDataSize) const uint8_t* ddsData,
-                                                 _In_ size_t ddsDataSize,
-                                                 _Outptr_opt_ ID3D11Resource** texture,
-                                                 _Outptr_opt_ ID3D11ShaderResourceView** textureView,
-                                                 _Outptr_ void** grfxMemory,
-                                                 _Out_opt_ DDS_ALPHA_MODE* alphaMode = nullptr, 
-                                                 _In_ bool forceSRGB = false
+    HRESULT __cdecl CreateDDSTextureFromMemory( _In_ ID3D11DeviceX* d3dDevice,
+                                                _In_reads_bytes_(ddsDataSize) const uint8_t* ddsData,
+                                                _In_ size_t ddsDataSize,
+                                                _Outptr_opt_ ID3D11Resource** texture,
+                                                _Outptr_opt_ ID3D11ShaderResourceView** textureView,
+                                                _Outptr_ void** grfxMemory,
+                                                _Out_opt_ DDS_ALPHA_MODE* alphaMode = nullptr, 
+                                                _In_ bool forceSRGB = false
                                                );
 
     HRESULT __cdecl CreateDDSTextureFromFile( _In_ ID3D11DeviceX* d3dDevice,
