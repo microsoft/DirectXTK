ac52f8e29b8bdf9911141a5436e987f42d01bc2e
Chuck Walbourn <chuckw@windows.microsoft.com>
2015-07-22 11:43:35 -0700

    Fix for #using and #include issues

--- Inc/Mouse.h
+++ Inc/Mouse.h
@@ -27,7 +27,7 @@
 #include <memory>
 
 #if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
-#include <Windows.UI.Core.h>
+namespace ABI { namespace Windows { namespace UI { namespace Core { struct ICoreWindow; } } } }
 #endif
 
 
