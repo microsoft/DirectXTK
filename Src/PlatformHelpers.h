8c9d1eddfc15439672d6bb3a80d3843e233f581c
Chuck Walbourn <chuckw@windows.microsoft.com>
2015-11-17 16:22:32 -0800

    Platform helpers updates

--- Src/PlatformHelpers.h
+++ Src/PlatformHelpers.h
@@ -16,16 +16,34 @@
 #pragma warning(disable : 4324 4481)
 
 #include <exception>
+#include <memory>
 
 
 namespace DirectX
 {
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) : result(hr) {}
+
+        virtual const char* what() const override
+        {
+            static char s_str[64] = { 0 };
+            sprintf_s(s_str, "Failure with HRESULT of %08X", result);
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
     // Helper utility converts D3D API failures into exceptions.
     inline void ThrowIfFailed(HRESULT hr)
     {
         if (FAILED(hr))
         {
-            throw std::exception();
+            throw com_exception(hr);
         }
     }
 
@@ -48,6 +66,12 @@ namespace DirectX
 
 
     // Helper smart-pointers
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN10) || (defined(_XBOX_ONE) && defined(_TITLE)) || !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP)
+    struct virtual_deleter { void operator()(void* p) { if (p) VirtualFree(p, 0, MEM_RELEASE); } };
+#endif
+
+    struct aligned_deleter { void operator()(void* p) { _aligned_free(p); } };
+
     struct handle_closer { void operator()(HANDLE h) { if (h) CloseHandle(h); } };
 
     typedef public std::unique_ptr<void, handle_closer> ScopedHandle;
