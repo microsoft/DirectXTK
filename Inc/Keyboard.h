2e0dcb8f58a4bd349c71e28cc376505dba4266a2
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-11-29 17:53:15 -0800

    Input code reformat

--- Inc/Keyboard.h
+++ Inc/Keyboard.h
@@ -16,7 +16,7 @@
 #include <memory>
 #include <stdint.h>
 
-#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+#if (defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)) || (defined(_XBOX_ONE) && defined(_TITLE))
 namespace ABI { namespace Windows { namespace UI { namespace Core { struct ICoreWindow; } } } }
 #endif
 
@@ -459,7 +459,7 @@ namespace DirectX
         static void __cdecl ProcessMessage(UINT message, WPARAM wParam, LPARAM lParam);
 #endif
 
-#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+#if (defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)) || (defined(_XBOX_ONE) && defined(_TITLE))
         void __cdecl SetWindow(ABI::Windows::UI::Core::ICoreWindow* window);
 #ifdef __cplusplus_winrt
         void __cdecl SetWindow(Windows::UI::Core::CoreWindow^ window)
