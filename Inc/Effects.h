7a94faeb1c5196a7fcc704143867b86031542e78
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-12-08 12:32:59 -0800

    Remove unused debug code

--- Inc/Effects.h
+++ Inc/Effects.h
@@ -605,9 +605,6 @@ namespace DirectX
         void XM_CALLCONV SetConstantAlbedo(FXMVECTOR value);
         void __cdecl SetConstantMetallic(float value);
         void __cdecl SetConstantRoughness(float value);
-#ifdef DEBUG
-        void __cdecl SetDebugFlags(bool diffuse, bool D, bool F, bool G);
-#endif
 
         // Texture settings.
         void __cdecl SetSurfaceTextures(
