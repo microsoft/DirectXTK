40dd7c00e9dcba866c38423d570cb1cf37e4c5db
Chuck Walbourn <chuckw@windows.microsoft.com>
2018-02-23 23:27:20 -0800

    Sync DDS header

--- Src/dds.h
+++ Src/dds.h
@@ -43,8 +43,10 @@ struct DDS_PIXELFORMAT
 #define DDS_RGBA        0x00000041  // DDPF_RGB | DDPF_ALPHAPIXELS
 #define DDS_LUMINANCE   0x00020000  // DDPF_LUMINANCE
 #define DDS_LUMINANCEA  0x00020001  // DDPF_LUMINANCE | DDPF_ALPHAPIXELS
+#define DDS_ALPHAPIXELS 0x00000001  // DDPF_ALPHAPIXELS
 #define DDS_ALPHA       0x00000002  // DDPF_ALPHA
 #define DDS_PAL8        0x00000020  // DDPF_PALETTEINDEXED8
+#define DDS_PAL8A       0x00000021  // DDPF_PALETTEINDEXED8 | DDPF_ALPHAPIXELS
 #define DDS_BUMPDUDV    0x00080000  // DDPF_BUMPDUDV
 
 #ifndef MAKEFOURCC
@@ -125,6 +127,9 @@ extern __declspec(selectany) const DDS_PIXELFORMAT DDSPF_L16 =
 extern __declspec(selectany) const DDS_PIXELFORMAT DDSPF_A8L8 =
     { sizeof(DDS_PIXELFORMAT), DDS_LUMINANCEA, 0, 16, 0x00ff, 0x0000, 0x0000, 0xff00 };
 
+extern __declspec(selectany) const DDS_PIXELFORMAT DDSPF_A8L8_ALT =
+    { sizeof(DDS_PIXELFORMAT), DDS_LUMINANCEA, 0, 8, 0x00ff, 0x0000, 0x0000, 0xff00 };
+
 extern __declspec(selectany) const DDS_PIXELFORMAT DDSPF_A8 =
     { sizeof(DDS_PIXELFORMAT), DDS_ALPHA, 0, 8, 0x00, 0x00, 0x00, 0xff };
 
@@ -182,7 +187,7 @@ enum DDS_RESOURCE_DIMENSION
 // Subset here matches D3D10_RESOURCE_MISC_FLAG and D3D11_RESOURCE_MISC_FLAG
 enum DDS_RESOURCE_MISC_FLAG
 {
-   DDS_RESOURCE_MISC_TEXTURECUBE = 0x4L,
+    DDS_RESOURCE_MISC_TEXTURECUBE = 0x4L,
 };
 
 enum DDS_MISC_FLAGS2
@@ -215,7 +220,7 @@ struct DDS_HEADER_DXT10
     uint32_t        miscFlag; // see D3D11_RESOURCE_MISC_FLAG
     uint32_t        arraySize;
     uint32_t        miscFlags2; // see DDS_MISC_FLAGS2
-} ;
+};
 
 #pragma pack(pop)
 
