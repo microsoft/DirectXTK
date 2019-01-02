4d217a4b42f8c47a0aacc5494bf74fc145398cf3
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-01-02 12:32:51 -0800

    Added V2 version of SDKMESH_MATERIAL for PBR

--- Src/SDKMesh.h
+++ Src/SDKMesh.h
@@ -106,6 +106,8 @@ namespace DXUT
     // Hard Defines for the various structures
     //--------------------------------------------------------------------------------------
     const uint32_t SDKMESH_FILE_VERSION = 101;
+    const uint32_t SDKMESH_FILE_VERSION_V2 = 200;
+
     const uint32_t MAX_VERTEX_ELEMENTS = 32;
     const uint32_t MAX_VERTEX_STREAMS = 16;
     const uint32_t MAX_FRAME_NAME = 100;
@@ -297,6 +299,47 @@ namespace DXUT
         };
     };
 
+    struct SDKMESH_MATERIAL_V2
+    {
+        char    Name[MAX_MATERIAL_NAME];
+
+        // PBR materials
+        char    RMATexture[MAX_TEXTURE_NAME];
+        char    AlbetoTexture[MAX_TEXTURE_NAME];
+        char    NormalTexture[MAX_TEXTURE_NAME];
+        char    EmissiveTexture[MAX_TEXTURE_NAME];
+
+        float   Alpha;
+
+        char    Reserved[60];
+
+        union
+        {
+            uint64_t Force64_1;			//Force the union to 64bits
+        };
+        union
+        {
+            uint64_t Force64_2;			//Force the union to 64bits
+        };
+        union
+        {
+            uint64_t Force64_3;			//Force the union to 64bits
+        };
+
+        union
+        {
+            uint64_t Force64_4;			//Force the union to 64bits
+        };
+        union
+        {
+            uint64_t Force64_5;		    //Force the union to 64bits
+        };
+        union
+        {
+            uint64_t Force64_6;			//Force the union to 64bits
+        };
+    };
+
     struct SDKANIMATION_FILE_HEADER
     {
         uint32_t Version;
@@ -338,6 +381,7 @@ static_assert( sizeof(DXUT::SDKMESH_MESH) == 224, "SDK Mesh structure size incor
 static_assert( sizeof(DXUT::SDKMESH_SUBSET) == 144, "SDK Mesh structure size incorrect" );
 static_assert( sizeof(DXUT::SDKMESH_FRAME) == 184, "SDK Mesh structure size incorrect" );
 static_assert( sizeof(DXUT::SDKMESH_MATERIAL) == 1256, "SDK Mesh structure size incorrect" );
+static_assert( sizeof(DXUT::SDKMESH_MATERIAL_V2) == sizeof(DXUT::SDKMESH_MATERIAL), "SDK Mesh structure size incorrect" );
 static_assert( sizeof(DXUT::SDKANIMATION_FILE_HEADER) == 40, "SDK Mesh structure size incorrect" );
 static_assert( sizeof(DXUT::SDKANIMATION_DATA) == 40, "SDK Mesh structure size incorrect" );
 static_assert( sizeof(DXUT::SDKANIMATION_FRAME_DATA) == 112, "SDK Mesh structure size incorrect" );
