a04ba40a135632490c85944a551193b51cea96e1
Chuck Walbourn <chuckw@microsoft.com>
2020-01-22 00:40:04 -0800

    More work

--- Audio/SoundStreamInstance.cpp
+++ Audio/SoundStreamInstance.cpp
@@ -9,9 +9,28 @@
 //--------------------------------------------------------------------------------------
 
 #include "pch.h"
+#include "DirectXHelpers.h"
+#include "PlatformHelpers.h"
 #include "SoundCommon.h"
 
 using namespace DirectX;
+
+namespace
+{
+    const size_t DVD_SECTOR_SIZE = 2048;
+    const size_t MAX_BUFFER_COUNT = 3;
+
+    size_t ComputeAsyncPacketSize(_In_ const WAVEFORMATEX* wfx)
+    {
+        if (!wfx)
+            return 0;
+
+        // TODO - for now, just a quick calculation
+        size_t buffer = wfx->nBlockAlign * wfx->nChannels * wfx->nSamplesPerSec;
+        buffer = AlignUp(buffer, DVD_SECTOR_SIZE);
+        return buffer;
+    }
+}
 
 
 //======================================================================================
@@ -22,11 +41,15 @@ using namespace DirectX;
 class SoundStreamInstance::Impl : public IVoiceNotify
 {
 public:
-    Impl(_In_ AudioEngine* engine, _In_ WaveBank* waveBank, uint32_t index, SOUND_EFFECT_INSTANCE_FLAGS flags) :
+    Impl(_In_ AudioEngine* engine,
+        WaveBank* waveBank,
+        uint32_t index,
+        SOUND_EFFECT_INSTANCE_FLAGS flags) noexcept(false) :
         mBase(),
         mWaveBank(waveBank),
         mIndex(index),
-        mLooped(false)
+        mLooped(false),
+        mPacketSize(0)
     {
         assert(engine != nullptr);
         engine->RegisterNotify(this, true);
@@ -35,6 +58,24 @@ public:
         auto wfx = reinterpret_cast<WAVEFORMATEX*>(buff);
         assert(mWaveBank != nullptr);
         mBase.Initialize(engine, mWaveBank->GetFormat(index, wfx, sizeof(buff)), flags);
+
+        mBufferEnd.reset(CreateEventEx(nullptr, nullptr, 0, EVENT_MODIFY_STATE | SYNCHRONIZE));
+        mBufferRead.reset(CreateEventEx(nullptr, nullptr, CREATE_EVENT_MANUAL_RESET, EVENT_MODIFY_STATE | SYNCHRONIZE));
+        if (!mBufferEnd || !mBufferRead)
+        {
+            throw std::exception("CreateEvent");
+        }
+
+        mPacketSize = ComputeAsyncPacketSize(wfx);
+        if (!mPacketSize)
+        {
+            throw std::exception("Invalid packet size");
+        }
+
+        for (size_t j = 0; j < MAX_BUFFER_COUNT; ++j)
+        {
+             mBuffers[j].reset(new uint8_t[mPacketSize]);
+        }
     }
 
     virtual ~Impl() override
@@ -53,7 +94,7 @@ public:
     // IVoiceNotify
     virtual void __cdecl OnBufferEnd() override
     {
-        // TODO -
+        SetEvent(mBufferEnd.get());
     }
 
     virtual void __cdecl OnCriticalError() override
@@ -97,6 +138,14 @@ public:
     WaveBank*                       mWaveBank;
     uint32_t                        mIndex;
     bool                            mLooped;
+
+private:
+    ScopedHandle                    mBufferEnd;
+    ScopedHandle                    mBufferRead;
+
+    std::unique_ptr<uint8_t[]>      mBuffers[MAX_BUFFER_COUNT];
+
+    size_t                          mPacketSize;
 };
 
 
