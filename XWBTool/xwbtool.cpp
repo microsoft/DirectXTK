aa8112e09abc20160afbae1d4b1ef804014a8689
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-02-23 21:54:10 -0800

    xwbtool: changed -n switch to a more safe -y switch

--- XWBTool/xwbtool.cpp
+++ XWBTool/xwbtool.cpp
@@ -777,7 +777,7 @@ enum OPTIONS
     OPT_STREAMING,
     OPT_OUTPUTFILE,
     OPT_OUTPUTHEADER,
-    OPT_NOOVERWRITE,
+    OPT_OVERWRITE,
     OPT_COMPACT,
     OPT_NOCOMPACT,
     OPT_FRIENDLY_NAMES,
@@ -844,7 +844,7 @@ const SValue g_pOptions[] =
     { L"s",         OPT_STREAMING },
     { L"o",         OPT_OUTPUTFILE },
     { L"h",         OPT_OUTPUTHEADER },
-    { L"n",         OPT_NOOVERWRITE },
+    { L"y",         OPT_OVERWRITE },
     { L"c",         OPT_COMPACT },
     { L"nc",        OPT_NOCOMPACT },
     { L"f",         OPT_FRIENDLY_NAMES },
@@ -971,7 +971,7 @@ namespace
         wprintf(L"                       otherwise an in-memory bank is created\n");
         wprintf(L"   -o <filename>       output filename\n");
         wprintf(L"   -h <h-filename>     output C/C++ header\n");
-        wprintf(L"   -n                  do not overwrite output\n");
+        wprintf(L"   -y                  overwrite existing output file (if any)\n");
         wprintf(L"   -c                  force creation of compact wavebank\n");
         wprintf(L"   -nc                 force creation of non-compact wavebank\n");
         wprintf(L"   -f                  include entry friendly names\n");
@@ -1029,20 +1029,6 @@ namespace
         {
             wprintf(L" (%hs %u channels, %u-bit, %lu Hz)", GetFormatTagName(wave.data.wfx->wFormatTag), wave.data.wfx->nChannels, wave.data.wfx->wBitsPerSample, wave.data.wfx->nSamplesPerSec);
         }
-    }
-
-    bool FileExists(const wchar_t* pszFilename)
-    {
-        FILE *f = nullptr;
-        if (!_wfopen_s(&f, pszFilename, L"rb"))
-        {
-            if (f)
-                fclose(f);
-
-            return true;
-        }
-
-        return false;
     }
 }
 
@@ -1211,6 +1197,42 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
     if (~dwOptions & (1 << OPT_NOLOGO))
         PrintLogo();
 
+    // Determine output file name
+    if (!*szOutputFile)
+    {
+        auto pConv = conversion.begin();
+
+        wchar_t ext[_MAX_EXT];
+        wchar_t fname[_MAX_FNAME];
+        _wsplitpath_s(pConv->szSrc, nullptr, 0, nullptr, 0, fname, _MAX_FNAME, ext, _MAX_EXT);
+
+        if (_wcsicmp(ext, L".xwb") == 0)
+        {
+            wprintf(L"ERROR: Need to specify output file via -o\n");
+            return 1;
+        }
+
+        _wmakepath_s(szOutputFile, nullptr, nullptr, fname, L".xwb");
+    }
+
+    if (~dwOptions & (1 << OPT_OVERWRITE))
+    {
+        if (GetFileAttributesW(szOutputFile) != INVALID_FILE_ATTRIBUTES)
+        {
+            wprintf(L"ERROR: Output file %ls already exists, use -y to overwrite!\n", szOutputFile);
+            return 1;
+        }
+
+        if (*szHeaderFile)
+        {
+            if (GetFileAttributesW(szHeaderFile) != INVALID_FILE_ATTRIBUTES)
+            {
+                wprintf(L"ERROR: Output header file %ls already exists!\n", szHeaderFile);
+                return 1;
+            }
+        }
+    }
+
     // Gather wave files
     std::unique_ptr<uint8_t[]> entries;
     std::unique_ptr<char[]> entryNames;
@@ -1229,16 +1251,6 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
         // Load source image
         if (pConv != conversion.begin())
             wprintf(L"\n");
-        else if (!*szOutputFile)
-        {
-            if (_wcsicmp(ext, L".xwb") == 0)
-            {
-                wprintf(L"ERROR: Need to specify output file via -o\n");
-                return 1;
-            }
-
-            _wmakepath_s(szOutputFile, nullptr, nullptr, fname, L".xwb");
-        }
 
         wprintf(L"reading %ls", pConv->szSrc);
         fflush(stdout);
@@ -1453,24 +1465,6 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
     wprintf(L"writing %ls%ls wavebank %ls w/ %zu entries\n", (compact) ? L"compact " : L"", (dwOptions & (1 << OPT_STREAMING)) ? L"streaming" : L"in-memory", szOutputFile, waves.size());
     fflush(stdout);
 
-    if (dwOptions & (1 << OPT_NOOVERWRITE))
-    {
-        if (FileExists(szOutputFile))
-        {
-            wprintf(L"ERROR: Output file %ls already exists!\n", szOutputFile);
-            return 1;
-        }
-
-        if (*szHeaderFile)
-        {
-            if (FileExists(szHeaderFile))
-            {
-                wprintf(L"ERROR: Output header file %ls already exists!\n", szHeaderFile);
-                return 1;
-            }
-        }
-    }
-
     hFile.reset(safe_handle(CreateFileW(szOutputFile, GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr)));
     if (!hFile)
     {
@@ -1498,7 +1492,10 @@ int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
 
     data.dwFlags = (dwOptions & (1 << OPT_STREAMING)) ? BANKDATA::TYPE_STREAMING : BANKDATA::TYPE_BUFFER;
 
-    data.dwFlags |= BANKDATA::FLAGS_SEEKTABLES;
+    if (seekEntries > 0)
+    {
+        data.dwFlags |= BANKDATA::FLAGS_SEEKTABLES;
+    }
 
     if (dwOptions & (1 << OPT_FRIENDLY_NAMES))
     {
