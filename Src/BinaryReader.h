bc351378f3934eb56bc75ad1400f60b77106919e
Chuck Walbourn <chuckw@microsoft.com>
2026-04-20 10:31:36 -0700

    Added guard for 32-bit overflow in spritefont reading

--- Src/BinaryReader.h
+++ Src/BinaryReader.h
@@ -45,7 +45,11 @@ namespace DirectX
         {
             static_assert(std::is_standard_layout<T>::value, "Can only read plain-old-data types");
 
-            uint8_t const* newPos = mPos + sizeof(T) * elementCount;
+            uint64_t byteCount = uint64_t(sizeof(T)) * uint64_t(elementCount);
+            if (byteCount > UINT32_MAX)
+                throw std::overflow_error("ReadArray");
+
+            uint8_t const* newPos = mPos + static_cast<size_t>(byteCount);
 
             if (newPos < mPos)
                 throw std::overflow_error("ReadArray");
