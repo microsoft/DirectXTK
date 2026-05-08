0995ad8653423759c2b4b7891da26981a50aa1c7
Chuck Walbourn <chuckw@microsoft.com>
2026-05-07 22:30:50 -0700

    Minor code review feedback

--- Src/DDS.h
+++ Src/DDS.h
@@ -11,7 +11,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 //
-// http://go.microsoft.com/fwlink/?LinkId=248926
+// https://go.microsoft.com/fwlink/?LinkId=248926
 // https://go.microsoft.com/fwlink/?LinkId=248929
 // https://go.microsoft.com/fwlink/?LinkID=615561
 //--------------------------------------------------------------------------------------
