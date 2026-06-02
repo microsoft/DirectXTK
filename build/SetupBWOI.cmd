c69da6deaf8d202e92c2c1453d51c7fe9a273091
Chuck Walbourn <walbourn@users.noreply.github.com>
2026-06-02 12:57:37 -0700

    walbourn

--- build/SetupBWOI.cmd
+++ build/SetupBWOI.cmd
@@ -13,9 +13,73 @@ goto needconsole
 set GXDKEDITION=%2
 echo GXDKEDITION: %GXDKEDITION%
 
-set PCNUGET=%1\Microsoft.GDK.PC\
-if NOT EXIST %PCNUGET% goto missingpcnuget
+set CORENUGET=%1\Microsoft.GDK.Core\
+if EXIST %CORENUGET% goto newlayout
 
+set PCNUGET=%1\Microsoft.GDK.PC\
+if EXIST %PCNUGET% goto oldlayout
+
+goto missingpcnuget
+
+REM Use new layouts (October 2025 GDK and later)
+:newlayout
+
+set GameDK=%CORENUGET%native\bin\
+
+if %3.==PC. goto newlayoutpc
+if %3.==XboxOne. goto newlayoutxboxone
+if %3.==Scarlett. goto newlayoutscarlett
+goto needconsole
+
+:newlayoutpc
+set WINDOWSNUGET=%1\Microsoft.GDK.Windows\
+if NOT EXIST %WINDOWSNUGET% goto missingpcnuget
+
+set GameDKCoreLatest=%WINDOWSNUGET%native\%GXDKEDITION%\
+
+set ADDINCLUDE=%GameDKCoreLatest%windows\include
+REM arm64?
+set ADDBIN=%GameDKCoreLatest%windows\bin\x64;%CORENUGET%native\bin
+set ADDLIB=%GameDKCoreLatest%windows\lib\x64
+goto continuenew
+
+:newlayoutxboxone
+set XBOXNUGET=%1\Microsoft.GDK.Xbox.XboxOne\
+if NOT EXIST %XBOXNUGET% goto missingxboxnuget
+
+set GameDKXboxLatest=%XBOXNUGET%native\%GXDKEDITION%\
+
+set ADDINCLUDE=%GameDKXboxLatest%xbox\include\gen8;%GameDKXboxLatest%xbox\include
+set ADDBIN=%GameDKXboxLatest%xbox\bin\gen8;%GameDKXboxLatest%xbox\bin\x64;%CORENUGET%native\bin
+set ADDLIB=%GameDKXboxLatest%xbox\lib\gen8;%GameDKXboxLatest%xbox\lib\x64
+goto continuenew
+
+:newlayoutscarlett
+set XBOXNUGET=%1\Microsoft.GDK.Xbox.XboxSeriesX_S\
+if NOT EXIST %XBOXNUGET% goto missingxboxnuget
+
+set GameDKXboxLatest=%XBOXNUGET%native\%GXDKEDITION%\
+
+set ADDINCLUDE=%GameDKXboxLatest%xbox\include\gen9;%GameDKXboxLatest%xbox\include
+set ADDBIN=%GameDKXboxLatest%xbox\bin\gen9;%GameDKXboxLatest%xbox\bin\x64;%CORENUGET%native\bin
+set ADDLIB=%GameDKXboxLatest%xbox\lib\gen9;%GameDKXboxLatest%xbox\lib\x64
+goto continuenew
+
+:continuenew
+echo GameDK: %GameDK%
+echo GameDKCoreLatest: %GameDKCoreLatest%
+echo GameDKXboxLatest: %GameDKXboxLatest%
+echo ADDBIN: %ADDBIN%
+echo ADDINCLUDE: %ADDINCLUDE%
+echo ADDLIB: %ADDLIB%
+
+set PATH=%ADDBIN%;%PATH%
+set INCLUDE=%INCLUDE%;%ADDINCLUDE%
+set LIB=%LIB%;%ADDLIB%
+exit /b 0
+
+REM Use old layouts (pre-October 2025 GDK)
+:oldlayout
 set GRDKLatest=%PCNUGET%native\%GXDKEDITION%\GRDK\
 echo GRDKLatest: %GRDKLatest%
 
@@ -33,7 +97,7 @@ set GameDKLatest=%XBOXNUGET%native\%GXDKEDITION%\
 set ADDBIN=%GXDKLatest%bin\%3;%PCNUGET%native\bin;%XBOXNUGET%native\bin
 set ADDINCLUDE=%GXDKLatest%gamekit\include\%3;%GXDKLatest%gamekit\include;%GRDKLatest%gamekit\include
 set ADDLIB=%GXDKLatest%gamekit\lib\amd64\%3;%GXDKLatest%gamekit\lib\amd64;%GRDKLatest%gamekit\lib\amd64
-goto continue
+goto continueold
 
 :grdkonly
 set GameDK=%PCNUGET%native\
@@ -43,7 +107,7 @@ set ADDBIN=%PCNUGET%native\bin
 set ADDINCLUDE=%GRDKLatest%gamekit\include
 set ADDLIB=%GRDKLatest%gamekit\lib\amd64
 
-:continue
+:continueold
 echo GameDK: %GameDK%
 echo GameDKLatest: %GameDKLatest%
 echo ADDBIN: %ADDBIN%
@@ -68,9 +132,9 @@ echo Usage: This script requires the target type of PC, Scarlett, or XboxOne in 
 exit /b 1
 
 :missingpcnuget
-echo ERROR - Cannot find Microsoft.GDK.PC installed at '%1'
+echo ERROR - Cannot find Microsoft.GDK.Core/Windows/PC installed at '%1'
 exit /b 1
 
 :missingxboxnuget
-echo ERROR - Cannot find Microsoft.GDK.Xbox installed at '%1'
+echo ERROR - Cannot find Microsoft.GDK.Xbox/.XboxOne/.XboxSeriesX_S installed at '%1'
 exit /b 1
