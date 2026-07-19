80c3eca855acf7f79a7d1e1a125e0f70cb223021
Chuck Walbourn <chuck_walbourn@yahoo.com>
2026-07-19 12:25:25 -0700

    Code review

--- Src/DebugEffect.cpp
+++ Src/DebugEffect.cpp
@@ -279,22 +279,13 @@ void DebugEffect::Impl::Apply(_In_ ID3D11DeviceContext* deviceContext)
     assert(deviceContext != nullptr);
 
     // Compute derived parameter values.
-    matrices.SetConstants(dirtyFlags, constants.worldViewProj);
-
-    // World inverse transpose matrix.
-    if (dirtyFlags & EffectDirtyFlags::WorldInverseTranspose)
-    {
-        constants.world = XMMatrixTranspose(matrices.world);
-
-        const XMMATRIX worldInverse = XMMatrixInverse(nullptr, matrices.world);
-
-        constants.worldInverseTranspose[0] = worldInverse.r[0];
-        constants.worldInverseTranspose[1] = worldInverse.r[1];
-        constants.worldInverseTranspose[2] = worldInverse.r[2];
-
-        dirtyFlags &= ~EffectDirtyFlags::WorldInverseTranspose;
-        dirtyFlags |= EffectDirtyFlags::ConstantBuffer;
-    }
+    XMVECTOR eyePosition{};
+    matrices.SetConstants(
+        dirtyFlags,
+        constants.world,
+        constants.worldInverseTranspose,
+        constants.worldViewProj,
+        eyePosition);
 
     // Set shaders and constant buffers.
     ApplyShaders(deviceContext, GetCurrentShaderPermutation());
--- Src/NPREffect.cpp
+++ Src/NPREffect.cpp
@@ -396,35 +396,12 @@ void NPREffect::Impl::Apply(_In_ ID3D11DeviceContext* deviceContext)
     assert(deviceContext != nullptr);
 
     // Compute derived parameter values.
-    matrices.SetConstants(dirtyFlags, constants.worldViewProj);
-
-    // World inverse transpose matrix.
-    if (dirtyFlags & EffectDirtyFlags::WorldInverseTranspose)
-    {
-        constants.world = XMMatrixTranspose(matrices.world);
-
-        const XMMATRIX worldInverse = XMMatrixInverse(nullptr, matrices.world);
-
-        constants.worldInverseTranspose[0] = worldInverse.r[0];
-        constants.worldInverseTranspose[1] = worldInverse.r[1];
-        constants.worldInverseTranspose[2] = worldInverse.r[2];
-
-        dirtyFlags &= ~EffectDirtyFlags::WorldInverseTranspose;
-        dirtyFlags |= EffectDirtyFlags::ConstantBuffer;
-    }
-
-    // Eye position.
-    if (dirtyFlags & EffectDirtyFlags::EyePosition)
-    {
-        const XMMATRIX viewInverse = XMMatrixInverse(nullptr, matrices.view);
-        constants.eyePosition = viewInverse.r[3];
-
-        dirtyFlags &= ~EffectDirtyFlags::EyePosition;
-        dirtyFlags |= EffectDirtyFlags::ConstantBuffer;
-    }
-
-    // Set shaders and constant buffers.
-    ApplyShaders(deviceContext, GetCurrentShaderPermutation());
+    matrices.SetConstants(
+        dirtyFlags,
+        constants.world,
+        constants.worldInverseTranspose,
+        constants.worldViewProj,
+        constants.eyePosition);
 
     // Set texture.
     switch (nprMode)
@@ -450,6 +427,9 @@ void NPREffect::Impl::Apply(_In_ ID3D11DeviceContext* deviceContext)
         }
         break;
     }
+
+    // Set shaders and constant buffers.
+    ApplyShaders(deviceContext, GetCurrentShaderPermutation());
 }
 
 
